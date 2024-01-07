a97b8ce82d1d6f759f48f12c6bc98afc96d32f15
Claussen <morten@setsnail.com>
2024-01-07 14:36:49 +0100

    Solved day 6 part 2

--- 2023/rust/day_6/src/main.rs
+++ 2023/rust/day_6/src/main.rs
@@ -10,7 +10,7 @@ impl Race {
     fn new(time: u64, distance: u64) -> Race {
         Race { time, distance }
     }
-    fn num_of_possible_records(&self) -> u64 {
+    fn num_of_wins(&self) -> u64 {
         let mut records = 0;
         for hold_time in 0..self.time {
             let travel_time = self.time - hold_time;
@@ -47,11 +47,19 @@ impl Track {
         Track { races }
     }
 
-    fn get_num_of_records(&self) -> Vec<u64> {
+    fn get_num_of_wins(&self) -> Vec<u64> {
         self.races
             .iter()
-            .map(|x| x.num_of_possible_records())
+            .map(|x| x.num_of_wins())
             .collect::<Vec<u64>>()
+    }
+
+    fn get_total_num_of_wins(&self) -> u64 {
+        self.get_num_of_wins()
+            .iter()
+            .copied()
+            .reduce(|a, b| a * b)
+            .unwrap()
     }
 }
 
@@ -65,24 +73,20 @@ fn main() {
     let result = part_one(&data);
     println!("Part one: {:?}", result);
 
-    // let result = part_two(&data);
-    // println!("Part two: {:?}", result);
+    let result = part_two(&data);
+    println!("Part two: {:?}", result);
 }
 
 fn part_one(data: &String) -> u64 {
     let race = Track::new(data.to_string());
-    let records = race
-        .get_num_of_records()
-        .iter()
-        .copied()
-        .reduce(|a, b| a * b)
-        .unwrap();
-    records
+    race.get_total_num_of_wins()
 }
 
-// fn part_two(data: &String) -> u64 {
-//     0
-// }
+fn part_two(data: &String) -> u64 {
+    let data = data.replace(" ", "");
+    let race = Track::new(data.to_string());
+    race.get_total_num_of_wins()
+}
 
 /**
 * NOTE: Tests
@@ -98,14 +102,12 @@ Distance:  9  40  200";
     #[test]
     fn test_part_one() {
         let result = part_one(&TEST_DATA.to_string());
-
         assert_eq!(result, 288);
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     let result = part_two(&TEST_DATA.to_string());
-    //
-    //     assert_eq!(result, 46);
-    // }
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&TEST_DATA.to_string());
+        assert_eq!(result, 71503);
+    }
 }
